479b8caae8ad08866346c530dfeef19d7a672478
Yuvraj <yv96875@gmail.com>
2023-01-22 17:07:00 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(4, 3);
+        p.product(4, 3, 6);
+        p.product(3.0, 4.0);
+    }
 
+    static class Product {
+        public static int product(int x, int y) {
+            return x * y;
+        }
+
+        public static int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public static double product(double x, double y) {
+
+            return x * y;
+        }
+
+    }
 }
